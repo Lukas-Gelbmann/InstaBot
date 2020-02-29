eab4b6907fb047f123a29bb766e3778b1e2ca56c
Lukas-Gelbmann <lukas.gelbmann@gmx.at>
2020-02-29 15:40:45 +0100

    added git ignore

--- instapostapi.py
+++ instapostapi.py
@@ -1,7 +1,20 @@
 from instagram_private_api import Client, ClientCompatPatch
+import os
+
 
 email = 'owexactly00@protonmail.com'
 password = 'mM9cjG9EpZaATis'
+picname = '1'
+
+
+programpath = os.path.dirname(__file__)
+path = programpath + '/pictures/' + picname + '.jpg'
+
+with open(path, "rb") as image:
+    f = image.read()
+    b = bytearray(f)
+    print(b[0])
+
 
 api = Client(email, password)
-api.post_photo(photo_data, size, caption='', upload_id=None, to_reel=False, **kwargs)
+api.post_photo(photo_data, size, caption='lit af', upload_id=None, to_reel=False)
